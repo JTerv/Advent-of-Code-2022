837a273ef560acade99b8aef911bf147db3e3825
Jack Tervay <jacktervay96@gmail.com>
2022-12-16 17:55:26 -0500

    Complete part 2 of day01

--- day01/day01.java
+++ day01/day01.java
@@ -4,13 +4,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.io.BufferedReader;
 
 class day01 {
-    public static void main(String[] args) {
-        BufferedReader reader;
-        ArrayList<Integer> calorieSums = new ArrayList<Integer>();
-        int tempSum = 0;
+
+    static BufferedReader reader;
+    static ArrayList<Integer> calorieSums = new ArrayList<Integer>();
+    static int tempSum = 0;
+
+    static int FindMaxCalories(int numberOfElfs) {
+        List<Integer> topCalories = new ArrayList<Integer>();
+        int topCaloriesSum = 0;
 
         try {
 
@@ -33,11 +38,19 @@ class day01 {
             }
 
             reader.close();
-            System.out.println("ARRAY: " + calorieSums);
-            System.out.println("MAXIMUM VALUE (ANSWER): " + Collections.max(calorieSums));
-            
+
+            Collections.sort(calorieSums);
+            topCalories = calorieSums.subList(calorieSums.size() - numberOfElfs, calorieSums.size());
+            topCaloriesSum = topCalories.stream().mapToInt(Integer::intValue).sum();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return topCaloriesSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FindMaxCalories(3));
     }
 }
